1380478cb565aaa2aca1554d89be8f8a9dd8bde1
michael <andrade.michael.e@gmail.com>
2016-10-21 11:46:23 -0700

    add javascript to capture radio button input

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
 $( document ).ready(function() {
-    
+  $("form#trackQuizForm").submit(function() {
+    event.preventDefault();
+    alert("It's working so far.");
+    var creativityAnswer = $("input:radio[name=creativity]:checked").val();
+    console.log(creativityAnswer);
+    var riskAnswer = $("input:radio[name=risk]:checked").val();
+    var frontOrBackAnswer = $("input:radio[name=frontOrBack]:checked").val();
+    var osAllegienceAnswer = $("input:radio[name=osAllegience]:checked").val();
+    var randomAnswer = $("input:radio[name=randomQuestion]:checked").val();
+    // $("#output").text(result);
+    // if (creativityAnswer === 1)
+  });
 });
